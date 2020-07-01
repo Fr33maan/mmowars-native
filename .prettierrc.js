e36678810791437741c3d4d5c63a17f1ef884dad
Fr33maan <coo@kapital.games>
2020-07-01 13:54:03 +0700

    MISC - config for prettier & standard

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
   singleQuote: true,
   trailingComma: 'all',
-};
+  semi: false,
+  allowParens: 'avoid',
+  printWidth: 1000,
+  tabWidth: 2,
+  useTabs: true,
+}
