c748349bd0295eaa8165ea3dc82b92f3cafe69b0
Fr33maan <coo@kapital.games>
2020-07-28 21:41:54 +0700

    PKG - RN Gesture handler

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 /**
  * @format
  */
-
-import {
-  AppRegistry
-} from 'react-native'
+import 'react-native-gesture-handler'
+import { AppRegistry } from 'react-native'
 import DeviceInfo from 'react-native-device-info'
 import TestApp from './App/testApp'
 import ProdApp from './App/prodApp'
-import {
-  name as appName
-} from './app.json'
+import { name as appName } from './app.json'
 
 const App = DeviceInfo.getBundleId().match('nativetesting') ? TestApp : ProdApp
 
